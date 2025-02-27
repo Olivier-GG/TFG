5d494c18394a170287dba2d47501fced5bf6b451
Olivier-GG <oliviegg@gmail.com>
2025-02-27 14:07:42 +0100

    Reestructuración del código para mejorar la legibilidad

--- cliente.py
+++ cliente.py
@@ -3,7 +3,6 @@ import os
 import sys
 import random
 from spawn_npc import spawnearCoches
-from spawnearNPC import Spawn
 import numpy as np
 import glob
 import os
@@ -17,6 +16,7 @@ import cv2
 import gymnasium as gym
 from gymnasium import spaces
 from CarlaEnv import CarlaEnv
+from FuncionesEntrenamientoFase1 import train_agent, evaluate_agent
 
 # Encontrar modulo de carla
 try:
@@ -119,6 +119,7 @@ def spawnearVehiculoAutonomo (world, blueprint_library, env): #Se le pasa el mun
 
     #||||||||||||||||||||||
     #Activamos los sensores
+    #||||||||||||||||||||||
 
     #camara.listen(lambda image: procesarImagen(image)) # Para activar la vista en primera persona
     camara.listen(lambda image: env.manejarSensorCamara(world, vehiculoAutonomo)) #En vez de procesar lo recibido por el sensor, se mueve al espectador para que siga al coche
@@ -132,14 +133,7 @@ def spawnearVehiculoAutonomo (world, blueprint_library, env): #Se le pasa el mun
 
 
 
-
-
 # |||||||| Funciones auxiliares ||||||||||
-
-# Guardar en JSON
-def guardar_qtable(q_table, filename):
-    with open("TablasFase1/" + filename + ".json", "w") as f:
-        json.dump(q_table.tolist(), f)
 
 # Cargar desde JSON
 def cargar_qtable(filename):
@@ -154,36 +148,12 @@ def initialize_q_table(state_space, action_space):
 
 
 
-#|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-#Funciones para la political de exploracion y explotacion
-
-def greedy_policy(Qtable, state):
-  # Exploitation: take the action with the highest state, action value
-  action = np.argmax(Qtable[state][:])
-
-  return action
-
-def epsilon_greedy_policy(Qtable, state, epsilon, env):
-  # Randomly generate a number between 0 and 1
-  random_num = random.uniform(0,1)
-  # if random_num > greater than epsilon --> exploitation
-  if random_num > epsilon:
-    # Take the action with the highest value given a state
-    # np.argmax can be useful here
-    action = greedy_policy(Qtable, state)
-  # else --> exploration
-  else:
-    action = env.action_space.sample()
-
-  return action
-
-
-
-
 #||||||||||||||||||||||||||||||||||||||||||||||||
 #||||||||||||||||||||||||||||||||||||||||||||||||
 #||||||||||||||||||||||||||||||||||||||||||||||||
-# |||||||||||||| MAIN |||||||||||||||||
+#||||||||||||||| MAIN |||||||||||||||||||||||||||
+#||||||||||||||||||||||||||||||||||||||||||||||||
+#||||||||||||||||||||||||||||||||||||||||||||||||
 def main () :
 
     try:
@@ -208,7 +178,6 @@ def main () :
         Qtable = initialize_q_table(state_space, action_space)
 
         print("Conexion con el servidor establecida y todas las variables principales inicializadas")
-
 
 
         #|||||||||||||||||| Parametros para el entrenamiento |||||||||||||||||
@@ -241,13 +210,12 @@ def main () :
         listaNPC.extend(spawnearCoches(30,10)) #Codigo de ejemplo carla 
 
 
-
         #Paso 3, elegir si queremos evaluar o entrenar agente
 
-        print("Introduce una 't' si quieres entrenar el agente o una 'e' si quieres evaluarlo: ")
+        print("\nIntroduce una 't' si quieres entrenar el agente o una 'e' si quieres evaluarlo(Seleccionar en el codigo que Qtable desea cargar -línea 221-): ")
         eleccion = input()
 
-        if eleccion == "e":
+        if eleccion == "e" or eleccion == "E":
 
             print("Evaluando agente...")
             Qtable = cargar_qtable("V2-3000")
@@ -260,15 +228,20 @@ def main () :
             print("Std reward: ", std_reward)
 
             
+        elif eleccion == "t" or eleccion == "T":
 
-        elif eleccion == "t":
-
-            print("\n\n\n")
+            
             print("               Comenzando el entrenamiento        \n")
 
             train_agent(env, Qtable, n_training_episodes, max_steps, gamma, learning_rate, max_epsilon, min_epsilon, decay_rate, Qtable, max_epsilon)
 
+            print("\n\n\n")
+            print("               Entrenamiento completado         \n")
 
+        else:
+            print("Opción no válida")
+
+        #Cerramos el enviroment
         env.close()
         print("Enviroment cerrado")
 
@@ -282,99 +255,13 @@ def main () :
 
 
 
-#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-#||||| Funciones para el agente ||||| |||||||||||||||||||||||
-#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-def train_agent(env, Q, n_training_episodes, max_steps, gamma, learning_rate, epsilon, min_epsilon, decay_rate, Qtable, max_epsilon):
-
-    for episode in range(n_training_episodes):
-        # Reduce epsilon (because we need less and less exploration)
-        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
-
-        #Printemaos al final de cada episodio para ver como va la Qtable
-        print("\n\n |||||||||||||||||||||||||||||")
-        print("Episodio: " + str(episode))
-        print ("Epsilon: " + str(epsilon))
-        print(Qtable)
-        print("||||||||||||||||||||||||||||||| \n\n")
-
-        # Reset the environment
-        info, state = env.reset()
-        step = 0
-        terminated = False
-
-        #Cada 200 episodios guardamos la Qtable para ver la evolución
-        if episode % 200 == 0:
-            guardar_qtable(Qtable, "V2-" + str(episode))
-
-        # repeat
-        for step in range(max_steps):
-            # Elegimos la accion segun nuestra politica
-            action = epsilon_greedy_policy(Qtable, state, epsilon, env)
-            #print( "Action: " + str(action))
-            # Take action At and observe Rt+1 and St+1
-            # Take the action (a) and observe the outcome state(s') and reward (r)
-            info, new_state, reward, terminated = env.step(action)
-
-            # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
-            Qtable[state][action] = Qtable[state][action] + learning_rate * (reward + gamma * np.max(Qtable[new_state]) - Qtable[state][action])
-
-            # If terminated or truncated finish the episode
-            if terminated:
-                break
-
-            # Our next state is the new state
-            state = new_state
-        
-    print("Entrenamiento finalizado, puede proceder a evaluarlo")
-
-
-
-# Evaluate the agent
-def evaluate_agent(env, max_steps, n_eval_episodes, Q):
-
-    episode_rewards = []
-
-    for episode in range(n_eval_episodes):
-
-        info, state = env.reset()
-        step = 0
-        terminated = False
-        total_rewards_ep = 0
-
-        for step in range(max_steps):
-            
-            action = greedy_policy(Q, state)
-            info, new_state, reward, terminated = env.step(action)
-            total_rewards_ep += reward
-            
-            if terminated:
-                break
-            
-            state = new_state
-
-            episode_rewards.append(total_rewards_ep)
-
-        print("Reward: " + str(total_rewards_ep))
-
-    mean_reward = np.mean(episode_rewards)
-    std_reward = np.std(episode_rewards)
-
-    return mean_reward, std_reward
-
-
-
-
-
-
-
-# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # |||||||||| Funciones para destruir los actores ||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 def destruirNPC():
     if len(listaNPC) > 0:
